9fde419bce761d3cfd0c2b58c0a26108aa3bb2db
Robert Wallis <smilingrob@gmail.com>
2018-12-16 16:40:04 -0800

    Docs, formatting.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
+// Copyright (C) 2018 Robert A. Wallis, all rights reserved.
 use std::{env, error, fmt, fs, io};
 
+/// Checks the program args, as search paramaters.  And then runs a search on the file.
 pub fn run() -> Result<(), GrepError> {
-    let params = SearchParams::from_args(env::args().collect())?;
+    let args: Vec<String> = env::args().collect();
+    let params = SearchParams::from_args(&args)?;
     search(&params)?;
     Ok(())
 }
 
+/// Given some search parameters, runs a search on the file.
 pub fn search(params: &SearchParams) -> Result<(), GrepError> {
     match fs::read_to_string(&params.filename) {
         Err(err) => Err(GrepError::IOError(params.filename.to_string(), err)),
@@ -17,15 +21,15 @@ pub fn search(params: &SearchParams) -> Result<(), GrepError> {
     }
 }
 
+/// Organized set of options for searching a file.
 pub struct SearchParams {
     pub term: String,
     pub filename: String,
 }
 
 impl SearchParams {
-    fn from_args(args: Vec<String>) -> Result<SearchParams, GrepError>
-    {
-        // let args: Vec<String> = args.collect();
+    /// Given &sys::env::args().collect(), from_args() returns a filled out SearchParams.
+    fn from_args(args: &[String]) -> Result<SearchParams, GrepError> {
         if args.len() < 3 {
             return Err(GrepError::NotEnoughParams);
         }
@@ -37,10 +41,13 @@ impl SearchParams {
 
 #[derive(Debug)]
 pub enum GrepError {
+    /// program needs more agurments to run correctly
     NotEnoughParams,
+    /// the OS returned a filesystem error
     IOError(String, io::Error),
 }
 
+/// Used by print to turn GrepError enum into a string.
 impl fmt::Display for GrepError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
@@ -53,6 +60,7 @@ impl fmt::Display for GrepError {
     }
 }
 
+/// Makes GrepError compatable with rust errors, so a function can use dyn Error type.
 impl error::Error for GrepError {}
 
 #[cfg(test)]
@@ -61,29 +69,38 @@ mod tests {
 
     #[test]
     fn search_params_zero_args() {
-        let params = SearchParams::from_args(Vec::new());
+        let params = SearchParams::from_args(&Vec::new());
         assert!(params.is_err());
     }
     #[test]
     fn search_params_just_prog() {
-        let params = SearchParams::from_args(vec!["prog".to_string()]);
+        let params = SearchParams::from_args(&vec!["prog".to_string()]);
         assert!(params.is_err());
     }
     #[test]
     fn search_params_one_arg() {
-        let params = SearchParams::from_args(vec!["prog".to_string(), "two".to_string()]);
+        let params = SearchParams::from_args(&vec!["prog".to_string(), "two".to_string()]);
         assert!(params.is_err());
     }
     #[test]
     fn search_params_two_args() {
-        let params = SearchParams::from_args(vec!["prog".to_string(), "two".to_string(), "three".to_string()]);
+        let params = SearchParams::from_args(&vec![
+            "prog".to_string(),
+            "two".to_string(),
+            "three".to_string(),
+        ]);
         let params = params.unwrap();
         assert_eq!(params.term, "two");
         assert_eq!(params.filename, "three");
     }
     #[test]
     fn search_params_three_args() {
-        let params = SearchParams::from_args(vec!["prog".to_string(), "two".to_string(), "three".to_string(), "four".to_string()]);
+        let params = SearchParams::from_args(&vec![
+            "prog".to_string(),
+            "two".to_string(),
+            "three".to_string(),
+            "four".to_string(),
+        ]);
         let params = params.unwrap();
         assert_eq!(params.term, "two three");
         assert_eq!(params.filename, "four");
